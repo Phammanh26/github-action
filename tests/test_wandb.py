5ea629d9743a92dcd9a2ea16c20c7dbaeb54a013
Pham vAN mANH <pvm26042000@gmail.com>
2022-04-13 00:53:55 +0700

    add_v1228823qssss111

--- tests/test_wandb.py
+++ tests/test_wandb.py
@@ -1,4 +1,13 @@
 import os
-print("api key: {}".format(os.getenv('WANDB_API_KEY')))
-print("api key: {}".format(os.getenv('WANDB_API_KEY')))
+import wandb
 os.environ["WANDB_API_KEY"] = os.getenv('WANDB_API_KEY')
+def test_wandb():
+    wandb.login()
+    wandb.init(project="my-awesome-hello")
+    wandb.alert(
+    title="Low accuracy", 
+    )  
+    wandb.log({'loss': 11111})
+
+if __name__ == '__main__':
+    test_wandb()
